fc9cf9e61dcda45b7af0b7e6280a20301235b619
Tupalski Ilya <itupalski@gmail.com>
2024-02-24 13:35:57 +0400

    feat: add dotenv (Sat Feb 24 13:35:57 +04 2024)

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const boardsActionHandler = require("./handlers/boardsActionHandler");
 const drawingHandler = require("./handlers/drawingHandler");
 
+const PORT = process.env.PORT || 5000;
+
 const httpServer = createServer();
 const io = new Server(httpServer, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: process.env.CORS_ORIGIN,
     },
 });
 function onConnection(socket) {
@@ -18,6 +21,6 @@ function onConnection(socket) {
 
 io.on("connection", onConnection);
 
-httpServer.listen(4000, () => {
-    console.log("Server start");
+httpServer.listen(PORT, () => {
+    console.log(`Server start on port ${PORT}`);
 });
